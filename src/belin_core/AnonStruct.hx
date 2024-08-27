b177b26b5e0e6feb2e8f0d7ccef91f656575b4d6
Cédric Belin <cedric@belin.io>
2024-08-27 23:29:47 +0200

    Add the `AnonStruct` class

---
+++ src/belin_core/AnonStruct.hx
@@ -0,0 +1,57 @@
+package belin_core;
+
+import haxe.DynamicAccess;
+using StringTools;
+
+#if macro
+import haxe.macro.Context;
+import haxe.macro.Expr;
+using haxe.macro.Tools;
+#end
+
+/** Provides helper methods for anonymous structures. **/
+abstract class AnonStruct {
+
+	/** Copies all properties from one or more source objects to the given `target` object. **/
+	public static function assign(target: DynamicAccess<Any>, ...sources: DynamicAccess<Any>): DynamicAccess<Dynamic> {
+		for (source in sources) for (key => value in source) target[key] = value;
+		return target;
+	}
+
+	/** Gets an array of the names of the fields in the specified structure. **/
+	macro public static function getNames(anonStruct: Expr, optionalPrefix: Bool = false)
+		return macro $a{getFields(anonStruct).map(field -> switch field.type {
+			case TAbstract(_.toString() => type, _) if (optionalPrefix && type == "Null"): macro $v{'?${field.name}'};
+			case _: macro $v{field.name};
+		})};
+
+	/** Recursively merges the properties from one or more source objects to the given `target` object. **/
+	public static function merge(target: DynamicAccess<Any>, ...sources: DynamicAccess<Any>): DynamicAccess<Dynamic> {
+		for (source in sources) for (key => value in source)
+			if (Type.typeof(value) == TObject && Type.typeof(target[key]) == TObject) merge(target[key], value);
+			else target[key] = value;
+
+		return target;
+	}
+
+	/** Removes whitespace from both ends of the string properties of the specified `object`. **/
+	public static function trim<T: {}>(object: T): T {
+		for (field in Reflect.fields(object)) {
+			final value = Reflect.field(object, field);
+			if (value is String) Reflect.setField(object, field, (value: String).trim());
+		}
+
+		return object;
+	}
+
+	#if macro
+	/** Gets an array of the fields in the specified structure. **/
+	static function getFields(anonStruct: Expr) {
+		final type = Context.getType(anonStruct.toString());
+		return switch type.follow() {
+			case TAnonymous(_.get() => type): type.fields;
+			case _: Context.error('"$type" should be an anonymous structure.', anonStruct.pos);
+		}
+	}
+	#end
+}
---
+++ test/belin_core/AnonStructTest.hx
@@ -0,0 +1,47 @@
+package core;
+
+import haxe.DynamicAccess;
+
+/** Tests the features of the `AnonStruct` class. **/
+@:asserts final class AnonStructTest {
+
+	/** Creates a new test. **/
+	public function new() {}
+
+	/** Tests the `assign()` method. **/
+	@:variant({}, {foo: "bar"}, {foo: "bar"})
+	@:variant({foo: "bar"}, {baz: "qux"}, {foo: "bar", baz: "qux"})
+	@:variant({foo: "bar", bar: {baz: "qux"}}, {foo: "123", bar: "456"}, {foo: "123", bar: "456"})
+	@:variant({foo: "123", bar: {baz: "456"}}, {foo: "ABC", bar: {qux: "DEF"}}, {foo: "ABC", bar: {qux: "DEF"}})
+	public function assign(target: DynamicAccess<Any>, source: DynamicAccess<Any>, output: DynamicAccess<Any>)
+		return compare(output, AnonStruct.assign(target, source));
+
+	/** Tests the `getNames()` method. **/
+	public function getNames() {
+		asserts.compare(["bool", "float", "integer", "string"], AnonStruct.getNames(SampleAnon, false));
+		asserts.compare(["bool", "?float", "integer", "?string"], AnonStruct.getNames(SampleAnon, true));
+		return asserts.done();
+	}
+
+	/** Tests the `merge()` method. **/
+	@:variant({}, {foo: "bar"}, {foo: "bar"})
+	@:variant({foo: "bar"}, {baz: "qux"}, {foo: "bar", baz: "qux"})
+	@:variant({foo: "bar", bar: {baz: "qux"}}, {foo: "123", bar: "456"}, {foo: "123", bar: "456"})
+	@:variant({foo: "123", bar: {baz: "456"}}, {foo: "ABC", bar: {qux: "DEF"}}, {foo: "ABC", bar: {baz: "456", qux: "DEF"}})
+	public function merge(target: DynamicAccess<Any>, source: DynamicAccess<Any>, output: DynamicAccess<Any>)
+		return compare(output, AnonStruct.merge(target, source));
+
+	/** Tests the `trim()` method. **/
+	@:variant({}, {})
+	@:variant({prop1: 123, prop2: " foo ", prop3: "\n456\n", prop4: " \t bar \t "}, {prop1: 123, prop2: "foo", prop3: "456", prop4: "bar"})
+	public function trim(input: {}, output: {})
+		return compare(output, AnonStruct.trim(input));
+}
+
+/** A sample anonymous structure. **/
+private typedef SampleAnon = {
+	var bool: Bool;
+	var ?float: Float;
+	var integer: Int;
+	var ?string: String;
+}
