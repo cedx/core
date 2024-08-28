c126ebed6def889f6e65af83f01785f2d1ae5650
Cédric Belin <cedric@belin.io>
2024-08-28 21:29:32 +0200

    Add the `LoadingIndicator` component

---
+++ src/belin_core/html/LoadingIndicator.hx
@@ -0,0 +1,40 @@
+package belin_core.html;
+
+import coconut.ui.Children;
+import coconut.ui.View;
+import js.Browser;
+
+/** A component that shows up when an HTTP request starts, and hides when all concurrent HTTP requests are completed. **/
+class LoadingIndicator extends View {
+
+	/** The view children. **/
+	@:children var nodes: Children;
+
+	/** Value indicating whether this indicator is hidden. **/
+	@:state var hidden = true;
+
+	/** The number of concurrent HTTP requests. **/
+	var requestCount = 0;
+
+	/** Starts the loading indicator. **/
+	public function start(): Void {
+		Browser.document.body.classList.add("loading");
+		Callback.defer(() -> hidden = false);
+		requestCount++;
+	}
+
+	/** Stops the loading indicator. **/
+	public function stop(force = false): Void {
+		requestCount--;
+		if (force || requestCount <= 0) {
+			Browser.document.body.classList.remove("loading");
+			Callback.defer(() -> hidden = true);
+			requestCount = 0;
+		}
+	}
+
+	/** Renders this view. **/
+	function render() '
+		<div hidden=$hidden>${...nodes}</div>
+	';
+}
