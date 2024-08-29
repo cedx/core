42b4d95b8430c1cca6437b5ce424ef2b3ffe6d8f
Cédric Belin <cedric@belin.io>
2024-08-29 12:46:44 +0200

    Add the `ActionBar` component

---
+++ src/belin_core/html/ActionBar.hx
@@ -0,0 +1,33 @@
+package core.html;
+
+import coconut.ui.Children;
+import coconut.ui.View;
+import js.Browser;
+import js.html.Element;
+
+/** An action bar located under the navigation bar. **/
+class ActionBar extends View {
+
+	/** The view children. **/
+	@:children var nodes: Children;
+
+	/** The root element. **/
+	@:ref final root: Element;
+
+	/** Renders this view. **/
+	function render() '
+		<aside class="container-fluid" ref=$root>
+			<div class="d-flex justify-content-between align-items-center">${...nodes}</div>
+		</aside>
+	';
+
+	/** Method invoked after this view is mounted. **/
+	override function viewDidMount(): Void {
+		var navbarHeight = Std.parseInt(Browser.window.getComputedStyle(Browser.document.documentElement).getPropertyValue("--navbar-height"));
+		if (navbarHeight == null) navbarHeight = 0;
+
+		final style = Browser.document.documentElement.style;
+		style.setProperty("--main-offset", '${navbarHeight + root.offsetHeight}px');
+		beforeUnmounting(() -> style.removeProperty("--main-offset"));
+	}
+}
