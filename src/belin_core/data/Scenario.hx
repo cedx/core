360ecccdcc9f5eee6d1a7e14bcded75bbfc2385e
Cédric Belin <cedric@belin.io>
2024-09-01 13:07:37 +0200

    Add the `Scenario` enumeration

---
+++ src/belin_core/data/Scenario.hx
@@ -0,0 +1,11 @@
+package belin_core.data;
+
+/** Defines the scenario used in data validation. **/
+enum Scenario {
+
+	/** A scenario in which the underlying model is created. **/
+	Creation;
+
+	/** A scenario in which the underlying model is updated. **/
+	Update;
+}
