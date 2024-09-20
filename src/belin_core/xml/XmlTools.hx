fe2bfa705462d21e7feab4f91379fd02119f1008
Cédric Belin <cedric@belin.io>
2024-09-21 00:44:14 +0200

    Add some typings

--- src/belin_core/xml/XmlTools.hx
+++ src/belin_core/xml/XmlTools.hx
@@ -6,14 +6,14 @@ using StringTools;
 abstract class XmlTools {
 
 	/** Adds a child element to the specified `element`. **/
-	public static function addChildElement(element: Xml, name: String, ?value: String, ?attributes: Map<String, String>) {
+	public static function addChildElement(element: Xml, name: String, ?value: String, ?attributes: Map<String, String>): Xml {
 		final child = createElement(name, value, attributes);
 		element.addChild(child);
 		return child;
 	}
 
 	/** Creates an XML element with the specified `name` and `value`. **/
-	public static function createElement(name: String, ?value: String, ?attributes: Map<String, String>) {
+	public static function createElement(name: String, ?value: String, ?attributes: Map<String, String>): Xml {
 		final element = Xml.createElement(name);
 		if (attributes != null) for (attrName => attrValue in attributes) element.set(attrName, attrValue);
 		if (value != null) element.addChild(Xml.createPCData(value.htmlEscape()));
