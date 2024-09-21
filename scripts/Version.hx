b48d907573c5d38902aa3679e0dd68f72c824105
Cédric Belin <cedric@belin.io>
2024-09-21 16:12:30 +0200

    Update the "Publish" script [skip ci]

--- scripts/Version.hx
+++ scripts/Version.hx
@@ -3,7 +3,5 @@ import belin_core.Platform;
 import belin_core.io.File;
 
 /** Updates the version number in the sources. **/
-function main() {
+function main()
 	new File("package.json").replace(~/"version": "\d+(\.\d+){2}"/, '"version": "${Platform.packageVersion}"');
-	new File("README.md").replace(~/project\/v\d+(\.\d+){2}/, 'project/v${Platform.packageVersion}');
-}
