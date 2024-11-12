9959a6ad47ea8eb955b0d2edce8d68464ecc3dc9
Cédric Belin <cedric@belin.io>
2024-11-13 00:55:36 +0100

    Update the typings [skip ci]

---
+++ lib/html/directive.d.ts
@@ -0,0 +1,8 @@
+import {TemplateResult} from "lit";
+
+/**
+ * Replaces all new lines in the specified value by HTML line breaks.
+ * @param value The string to format.
+ * @returns The formatted string.
+ */
+export function nl2br(value: string): Iterable<TemplateResult>;
