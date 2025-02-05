05310005e42c99ac7a51866c76b51e357296562b
Cédric Belin <cedric@belin.io>
2025-02-05 21:57:15 +0100

    Port the `DateRange` class

--- src/util/date_range.ts
+++ src/util/date_range.ts
@@ -1,3 +1,6 @@
+import {atMidnight, daysInMonth, getQuarter} from "./date.js";
+import {Duration} from "./duration.js";
+
 /**
  * Defines a date range.
  */
@@ -6,17 +9,17 @@ export class DateRange {
 	/**
 	 * The end date.
 	 */
-	end: Date;
+	readonly end: Date;
 
 	/**
 	 * The start date.
 	 */
-	start: Date;
+	readonly start: Date;
 
 	/**
 	 * The range type.
 	 */
-	type: DateRangeType;
+	readonly type: DateRangeType;
 
 	/**
 	 * Creates a new date range.
@@ -24,62 +27,97 @@ export class DateRange {
 	 * @param end The end date.
 	 * @param type The range type.
 	 */
-	constructor(start: Date, end: Date, type?: DateRangeType);
+	constructor(start: Date, end: Date, type: DateRangeType = DateRangeType.custom) {
+		this.start = start;
+		this.end = end;
+		this.type = type;
+	}
 
 	/**
 	 * Creates a new date range from the specified JSON object.
 	 * @param json A JSON object representing a date range.
 	 * @returns The instance corresponding to the specified JSON object.
 	 */
-	static fromJson(json: Record<string, any>): DateRange;
+	static fromJson(json: Record<string, any>): DateRange {
+		return new this(
+			new Date(typeof json.start == "string" ? json.start : Date.now()),
+			new Date(typeof json.end == "string" ? json.end : Date.now()),
+			Object.values(DateRangeType).includes(json.type as DateRangeType) ? json.type as DateRangeType : DateRangeType.custom
+		);
+	}
 
 	/**
 	 * Creates a date range corresponding to the day including the specified date.
 	 * @param date The date.
 	 * @returns The date range corresponding to the day including the specified date.
 	 */
-	static day(date: Date): DateRange;
+	static day(date: Date): DateRange {
+		return new this(atMidnight(date), new Date(date.getFullYear(), date.getMonth(), date.getDate(), 23, 59, 59), DateRangeType.day);
+	}
 
 	/**
 	 * Creates a date range corresponding to the month including the specified date.
 	 * @param date The date.
 	 * @returns The date range corresponding to the month including the specified date.
 	 */
-	static month(date: Date): DateRange;
+	static month(date: Date): DateRange {
+		return new this(
+			new Date(date.getFullYear(), date.getMonth(), 1),
+			new Date(date.getFullYear(), date.getMonth(), daysInMonth(date), 23, 59, 59),
+			DateRangeType.month
+		);
+	}
 
 	/**
 	 * Creates a date range corresponding to the quarter including the specified date.
 	 * @param date The date.
 	 * @returns The date range corresponding to the quarter including the specified date.
 	 */
-	static quarter(date: Date): DateRange;
+	static quarter(date: Date): DateRange {
+		const firstMonth = (getQuarter(date) * 3) - 3;
+		const lastMonth = firstMonth + 2;
+		return new this(
+			new Date(date.getFullYear(), firstMonth, 1),
+			new Date(date.getFullYear(), lastMonth, daysInMonth(new Date(date.getFullYear(), lastMonth, 1)), 23, 59, 59),
+			DateRangeType.quarter
+		);
+	}
 
 	/**
 	 * Creates a date range corresponding to the week including the specified date.
 	 * @param date The date.
 	 * @returns The date range corresponding to the week including the specified date.
 	 */
-	static week(date: Date): DateRange;
+	static week(date: Date): DateRange {
+		const delta = -((date.getDay() + 6) % 7);
+		const sunday = new Date(date.getTime() + ((delta + 6) * Duration.day));
+		sunday.setHours(23, 59, 59);
+		return new this(atMidnight(new Date(date.getTime() + (delta * Duration.day))), sunday, DateRangeType.week);
+	}
 
 	/**
 	 * Creates a date range corresponding to the year including the specified date.
 	 * @param date The date.
 	 * @returns The date range corresponding to the year including the specified date.
 	 */
-	static year(date: Date): DateRange;
+	static year(date: Date): DateRange {
+		return new this(new Date(date.getFullYear(), 0, 1), new Date(date.getFullYear(), 11, 31, 23, 59, 59), DateRangeType.year);
+	}
 
 	/**
 	 * Returns a value indicating whether this date range is equal to the specified date range.
 	 * @param other The other date range to compare.
 	 * @returns `true` if this date range is equal to the specified date range, otherwise `false`.
 	 */
-	equals(other: DateRange): boolean;
+	equals(other: DateRange): boolean {
+		return this.start.getTime() == other.start.getTime() && this.end.getTime() == other.end.getTime();
+	}
 }
 
 /**
  * Defines the type of a date range.
  */
-export const DateRangeType: Readonly<{
+export const DateRangeType = Object.freeze({
 
 	/**
 	 * A custom date range.
@@ -110,7 +148,7 @@ export const DateRangeType: Readonly<{
 	 * A whole year.
 	 */
 	year: "P1Y"
-}>;
+});
 
 /**
  * Defines the type of a date range.
---
+++ test/util/date_range_test.js
@@ -0,0 +1,91 @@
+import {DateRange, DateRangeType} from "@cedx/core/util/date_range.js";
+import {assert} from "chai";
+
+/**
+ * Tests the features of the {@link DateRange} class.
+ */
+describe("DateRange", () => {
+	const {equal} = assert;
+
+	describe("end", () => {
+		it("should return the last moment of the date range", () => {
+			const date = new Date("1974-05-03 08:45:12");
+			equal(new DateRange(date, new Date("2023-11-30 17:30:47")).end.getTime(), new Date("2023-11-30 17:30:47").getTime());
+			equal(DateRange.day(date).end.getTime(), new Date("1974-05-03 23:59:59").getTime());
+			equal(DateRange.week(date).end.getTime(), new Date("1974-05-05 23:59:59").getTime());
+			equal(DateRange.month(date).end.getTime(), new Date("1974-05-31 23:59:59").getTime());
+			equal(DateRange.quarter(date).end.getTime(), new Date("1974-06-30 23:59:59").getTime());
+			equal(DateRange.year(date).end.getTime(), new Date("1974-12-31 23:59:59").getTime());
+		});
+	});
+
+	describe("start", () => {
+		it("should return the first moment of the date range", () => {
+			const date = new Date("1974-05-03 08:45:12");
+			equal(new DateRange(date, new Date("2023-11-30 17:30:47")).start.getTime(), date.getTime());
+			equal(DateRange.day(date).start.getTime(), new Date("1974-05-03 00:00:00").getTime());
+			equal(DateRange.week(date).start.getTime(), new Date("1974-04-29 00:00:00").getTime());
+			equal(DateRange.month(date).start.getTime(), new Date("1974-05-01 00:00:00").getTime());
+			equal(DateRange.quarter(date).start.getTime(), new Date("1974-04-01 00:00:00").getTime());
+			equal(DateRange.year(date).start.getTime(), new Date("1974-01-01 00:00:00").getTime());
+		});
+	});
+
+	describe("type", () => {
+		it("should return the type of a date range", () => {
+			const date = new Date("1974-05-03 08:45:12");
+			equal(new DateRange(date, new Date("2023-11-30 17:30:47")).type, DateRangeType.custom);
+			equal(DateRange.day(date).type, DateRangeType.day);
+			equal(DateRange.week(date).type, DateRangeType.week);
+			equal(DateRange.month(date).type, DateRangeType.month);
+			equal(DateRange.quarter(date).type, DateRangeType.quarter);
+			equal(DateRange.year(date).type, DateRangeType.year);
+		});
+	});
+
+	describe("equals()", () => {
+		it("should return `true` if the two date ranges are equal", () => {
+			const date = new Date("1974-05-03 00:00:00");
+			assert.isTrue(new DateRange(date, new Date("2023-11-30 12:30:15")).equals(new DateRange(date, new Date("2023-11-30 12:30:15"))));
+			assert.isTrue(DateRange.day(date).equals(DateRange.day(date)));
+			assert.isTrue(DateRange.week(date).equals(DateRange.week(new Date("1974-04-29 00:00:00"))));
+			assert.isTrue(DateRange.month(date).equals(DateRange.month(new Date("1974-05-01 00:00:00"))));
+			assert.isTrue(DateRange.quarter(date).equals(DateRange.quarter(new Date("1974-04-01 00:00:00"))));
+			assert.isTrue(DateRange.year(date).equals(DateRange.year(new Date("1974-01-01 00:00:00"))));
+		});
+
+		it("should return `false` if the two date ranges are not equal", () => {
+			const date = new Date("1974-05-03 00:00:00");
+			assert.isFalse(new DateRange(date, new Date("2023-11-30 12:30:15")).equals(new DateRange(date, new Date("2022-10-15 12:30:15"))));
+			assert.isFalse(DateRange.day(date).equals(DateRange.day(new Date("1974-05-04 00:00:00"))));
+			assert.isFalse(DateRange.week(date).equals(DateRange.week(new Date("1974-05-08 00:00:00"))));
+			assert.isFalse(DateRange.month(date).equals(DateRange.month(new Date("1974-06-03 00:00:00"))));
+			assert.isFalse(DateRange.quarter(date).equals(DateRange.quarter(new Date("1974-09-03 00:00:00"))));
+			assert.isFalse(DateRange.year(date).equals(DateRange.year(new Date("1975-05-03 00:00:00"))));
+		});
+	});
+
+	describe("week()", () => {
+		it("should return the week encompassing the specified date", () => {
+			let range = DateRange.week(new Date("2015-12-31 00:00:00"));
+			equal(range.start.getTime(), new Date("2015-12-28 00:00:00").getTime());
+			equal(range.end.getTime(), new Date("2016-01-03 23:59:59").getTime());
+
+			range = DateRange.week(new Date("2017-07-14 00:00:00"));
+			equal(range.start.getTime(), new Date("2017-07-10 00:00:00").getTime());
+			equal(range.end.getTime(), new Date("2017-07-16 23:59:59").getTime());
+
+			range = DateRange.week(new Date("2020-05-03 00:00:00"));
+			equal(range.start.getTime(), new Date("2020-04-27 00:00:00").getTime());
+			equal(range.end.getTime(), new Date("2020-05-03 23:59:59").getTime());
+
+			range = DateRange.week(new Date("2023-01-01 00:00:00"));
+			equal(range.start.getTime(), new Date("2022-12-26 00:00:00").getTime());
+			equal(range.end.getTime(), new Date("2023-01-01 23:59:59").getTime());
+
+			range = DateRange.week(new Date("2024-02-29 00:00:00"));
+			equal(range.start.getTime(), new Date("2024-02-26 00:00:00").getTime());
+			equal(range.end.getTime(), new Date("2024-03-03 23:59:59").getTime());
+		});
+	});
+});
