0f097712416ab2bcc48370affc74713ed6e1c2a5
Cédric Belin <cedric@belin.io>
2025-02-07 09:57:39 +0100

    Porte the `ViewportScroller` class [skip ci]

--- src/html/viewport_scroller.d.ts
+++ src/html/viewport_scroller.d.ts
@@ -17,27 +17,57 @@ export class ViewportScroller {
 	/**
 	 * The top offset used when scrolling to an element.
 	 */
-	readonly scrollOffset: number;
+	#scrollOffset = -1;
+
+	/**
+	 * The function returning the element used as viewport.
+	 */
+	readonly #viewport: () => Element;
 
 	/**
 	 * Creates a new viewport scroller.
 	 * @param viewport A function that returns the element used as viewport.
 	 */
-	constructor(viewport?: () => Element);
+	constructor(viewport: () => Element = () => document.scrollingElement ?? document.documentElement) {
+		this.#viewport = viewport;
+	}
+
+	/**
+	 * The top offset used when scrolling to an element.
+	 */
+	get scrollOffset(): number {
+		if (this.#scrollOffset < 0) {
+			const fontSize = parseInt(getComputedStyle(document.body).fontSize);
+			this.#scrollOffset = Number.isNaN(fontSize) ? 0 : fontSize * 2;
+
+			const navbarHeight = parseInt(getComputedStyle(document.documentElement).getPropertyValue("--navbar-height"));
+			this.#scrollOffset += Number.isNaN(navbarHeight) ? 0 : navbarHeight;
+		}
+
+		const actionBar = document.body.querySelector<HTMLElement>("action-bar");
+		return this.#scrollOffset + (actionBar?.offsetHeight ?? 0);
+	}
 
 	/**
 	 * Scrolls to the specified anchor.
 	 * @param anchor The identifier or name of an elment.
 	 * @param options Value indicating whether scrolling is instant or animates smoothly.
 	 */
-	scrollToAnchor(anchor: string, options?: ScrollOptions): void;
+	scrollToAnchor(anchor: string, options: ScrollOptions = {}): void {
+		const element = document.getElementById(anchor) ?? document.body.querySelector(`[name="${anchor}"]`);
+		if (element) this.scrollToElement(element, options);
+	}
 
 	/**
 	 * Scrolls to the specified element.
 	 * @param element The element to scroll to.
 	 * @param options Value indicating whether scrolling is instant or animates smoothly.
 	 */
-	scrollToElement(element: Element, options?: ScrollOptions): void;
+	scrollToElement(element: Element, options: ScrollOptions = {}): void {
+		const {left, top} = element.getBoundingClientRect();
+		const {scrollLeft, scrollTop} = this.#viewport();
+		this.scrollToPosition(left + scrollLeft, top + scrollTop - this.scrollOffset, options);
+	}
 
 	/**
 	 * Scrolls to the specified position.
@@ -45,11 +75,15 @@ export class ViewportScroller {
 	 * @param y The pixel along the vertical axis.
 	 * @param options Value indicating whether scrolling is instant or animates smoothly.
 	 */
-	scrollToPosition(x: number, y: number, options?: ScrollOptions): void;
+	scrollToPosition(x: number, y: number, options: ScrollOptions = {}): void {
+		this.#viewport().scrollTo({left: x, top: y, behavior: options.behavior ?? "auto"});
+	}
 
 	/**
 	 * Scrolls to the top.
 	 * @param options Value indicating whether scrolling is instant or animates smoothly.
 	 */
-	scrollToTop(options?: ScrollOptions): void;
+	scrollToTop(options: ScrollOptions = {}): void {
+		this.scrollToPosition(0, 0, options);
+	}
 }
