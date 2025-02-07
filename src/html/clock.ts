62acad578fbd43a70c78e9d2d1be4b88c7f5f4bf
Cédric Belin <cedric@belin.io>
2025-02-07 10:13:29 +0100

    Port the `Clock` class

---
+++ src/html/clock.ts
@@ -0,0 +1,57 @@
+import type {ReactiveControllerHost} from "lit";
+import {Duration} from "../util/duration.js";
+
+/**
+ * A controller managing a clock.
+ */
+export class Clock {
+
+	/**
+	 * The clock value.
+	 */
+	value = new Date;
+
+	/**
+	 * The host element.
+	 */
+	readonly #host: ReactiveControllerHost;
+
+	/**
+	 * The timer delay, in milliseconds.
+	 */
+	readonly #timeout: number;
+
+	/**
+	 * The timer identifier.
+	 */
+	#timer = 0;
+
+	/**
+	 * Creates a new clock controller.
+	 * @param host The host element.
+	 * @param timeout The timer delay, in seconds.
+	 */
+	constructor(host: ReactiveControllerHost, timeout = 1) {
+		(this.#host = host).addController(this);
+		this.#timeout = timeout * Duration.second;
+	}
+
+	/**
+	 * Method invoked when the host element is connected.
+	 */
+	hostConnected(): void {
+		this.value = new Date;
+		this.#timer = window.setInterval(() => {
+			this.value = new Date;
+			this.#host.requestUpdate();
+		}, this.#timeout);
+	}
+
+	/**
+	 * Method invoked when the host element is disconnected.
+	 */
+	hostDisconnected(): void {
+		clearInterval(this.#timer);
+		this.#timer = 0;
+	}
+}
